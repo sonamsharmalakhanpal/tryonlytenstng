82c0535d0a3fa7fdad1580696d7b73175a87cd94
Kris Brown <Kris Brown>
2016-11-23 09:56:00 +0530

    jai Maa Parwati ji

---
+++ batchfile.bat
@@ -0,0 +1,3 @@
+cd C:\Users\meetg\workspace\trygetgethubjenkins
+java -cp C:/Users/meetg/workspace/trygetgethubjenkins/*;C:/Users/meetg/workspace/trygetgethubjenkins/bin org.testng.TestNG testng.xml
+pause
---
+++ src/trygetgethubjenkins/test1.java
@@ -0,0 +1,26 @@
+package trygetgethubjenkins;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+	@Test
+	public void testmethod(){
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(1L, TimeUnit.SECONDS);
+		driver.navigate().to("https://gmail.com/");
+		driver.findElement(By.id("Email")).sendKeys("sonam.sharma2407@gmail.com");
+		driver.findElement(By.id("next")).click();
+		driver.findElement(By.id("Passwd")).sendKeys("abcd");
+		driver.findElement(By.id("signIn")).click();
+	
+	}
+
+}
